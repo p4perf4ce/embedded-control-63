69337872249c7adff3bc5e486bed5a4fa02fdcab
Amrest Chinkamol <amrest.c@ku.th>
2021-03-13 14:15:09 +0700

    Added communicator
    Signed-off-by: Amrest Chinkamol <amrest.c@ku.th>

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 #include <mbed.h>
+#include "ssd1306.h"
 // #include <string>
 
 // PIN MAP
@@ -9,13 +13,21 @@
 #define LED_1 D1
 #define LED_2 D2
 #define LED_3 D3
+#define I2C_SDA D4
+#define I2C_SCL D5
+#define I2C_ESP_SDA D11
+#define I2C_ESP_SCL D12
 
 // CONST
 #define READ_INTERVAL 1.0F
-#define MAIN_THREAD_DELAY 1
+#define MAIN_THREAD_DELAY 100
 #define TRIGGER_THRESHOLD_1 80000
 #define TRIGGER_THRESHOLD_2 80000
 #define TRIGGER_THRESHOLD_3 80000
+#define BOOK_NAME_SIZE 255 //Numeric Char + Whitespace + MAX_VARCHAR_BOOK_NAME
+#define NUMERIC_CHAR_INDEX 0
+#define BOOK_NAME_INDEX 2
+#define MAX_BOOK 3
 
 // Interfaces
 class LightReader {
@@ -86,6 +98,54 @@ class LEDLoop {
         }
 };
 
+typedef struct Book {
+    volatile char book_name[256];
+    volatile bool* book_status;
+} Book;
+
+class ESPCommunicator {
+    private:
+        I2CSlave _slave;
+        Thread comm_thread;
+        volatile Book* book_array_ptr;
+        char buff[BOOK_NAME_INDEX + BOOK_NAME_SIZE + 1];
+        void communicator() {
+            while(true){
+                int i = _slave.receive();
+                switch (i) {
+                    /** NO CASE FOR THIS
+                    case I2CSlave::ReadAddressed: // Master force read.
+                        for(int j = 0; j<sizeof(buff); j++) buff[j] = 0; // **REFACTOR: NO
+                        break;
+                    **/
+                    case I2CSlave::WriteAddressed: // Master update book name.
+                        for(int j = 0; j<sizeof(buff); j++) buff[j] = 0; // **REFACTOR: NO
+                        _slave.read(buff, sizeof(buff)-1);
+                        char *end = &buff[BOOK_NAME_INDEX-1];
+                        int book_index = strtol(buff, &end, 10);
+                        if (book_index > MAX_BOOK - 1) {
+                            printf("INVALID SIGNATURE ACCEPTED. IGNORING READ ...\n");
+                            continue;
+                        }
+                        strcpy((char *) book_array_ptr[book_index].book_name, &buff[BOOK_NAME_INDEX]);
+                        break;
+                }
+            }
+        }
+    public:
+        ESPCommunicator(PinName SDA, PinName SCL, int slave_address, volatile Book* bookarray): _slave(SDA, SCL){
+            _slave.address(slave_address);
+            book_array_ptr = bookarray;
+        }
+        void start(){
+            // printf("Starting communicator ...\n");
+            comm_thread.start(callback(this, &ESPCommunicator::communicator));
+        }
+        void stop(){
+            comm_thread.terminate();
+        }
+};
+
 // Shared-Variable
 volatile float ldr_1_value;
 volatile float ldr_2_value;
@@ -100,37 +160,74 @@ LEDControl led_1(LED_1, &ldr_1.trigger_level);
 LEDControl led_2(LED_2, &ldr_2.trigger_level);
 LEDControl led_3(LED_3, &ldr_3.trigger_level);
 
+SSD1306 oled(I2C_SDA, I2C_SCL);
+
+
 // Read Control
 Ticker ldr_1_tick;
 Ticker ldr_2_tick;
 Ticker ldr_3_tick;
 
 // ASX
-EventQueue queue;
-Thread sub_thread_1;
+volatile Book shelf[MAX_BOOK];
+ESPCommunicator comm(I2C_ESP_SDA, I2C_ESP_SCL, 0x90, shelf);
 
 
-// USER INTERRUPTS
+// USER CODE
+void report_shelf_status(volatile Book* bPtr){
+    for(int book_index = 0; book_index < MAX_BOOK; book_index++){
+        printf("SLOT: %d, BOOK_NAME: %s, STATUS: %d\n", book_index, bPtr[book_index].book_name, *(bPtr[book_index].book_status));
+    }
+}
 
 
 // USER CODE MAIN LOOP
 int main() {
-    printf("System Started ...\n");
-    led.start();
-    // Begin here
+    // Initialization
+    // printf("System Started ...\n");
+    // printf("Perform System Check ..\n");
+    // led.start();
+    // // Oled Manager
+    // oled.speed(SSD1306::Medium);
+    // oled.init();
+    // oled.cls();
+    // oled.locate(3, 1);
+    // oled.printf ("TEST MODE 1"); // print to frame buffer
+    // oled.line (  6, 22, 114, 22, SSD1306::Normal); //
+    // oled.line (114, 22, 114, 33, SSD1306::Normal); // Surrounds text with 
+    // oled.line (114, 33,   6, 33, SSD1306::Normal); // a rectangle
+    // oled.line (  6, 33,   6, 22, SSD1306::Normal); //
+    // oled.fill (0, 0);
+    // // oled.display();
+    // oled.redraw();
 
-    // Start LDR Reader
-    ldr_1.start();
-    ldr_2.start();
-    ldr_3.start();
-    led_1.start();
-    led_2.start();
-    led_3.start();
+    // shelf[0].book_status = &ldr_1.trigger_level;
+    // shelf[1].book_status = &ldr_2.trigger_level;
+    // shelf[2].book_status = &ldr_3.trigger_level;
+    // report_shelf_status(shelf);
+    // printf("System Check Complete\n");
+    // End Initialization
 
-    // Main Loop
-    while(1){
-        // User Code Begin Here
-        // Main Thread hold out
-        ThisThread::sleep_for(MAIN_THREAD_DELAY);
-    }
+
+    // // User Code Begin here
+
+    // // Start LDR Reader
+    // ldr_1.start();
+    // ldr_2.start();
+    // ldr_3.start();
+    // // Start led controller
+    // led_1.start();
+    // led_2.start();
+    // led_3.start();
+    // // Start communicator
+    // // comm.start();
+
+
+    // // Main Loop
+    // while(1){
+    //     // User Code Begin Here
+    //     // Main Thread hold out
+    //     report_shelf_status(shelf);
+    //     ThisThread::sleep_for(MAIN_THREAD_DELAY);
+    // }
 }
